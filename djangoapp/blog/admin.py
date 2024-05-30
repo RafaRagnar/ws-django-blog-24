a26bd8a59439f28e9dab8303b8f8655e40ccf384
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-05-30 18:55:09 -0300

    Creating Tag, Category, Page, and Post Models, SlugField, Slug, and Slugify

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,3 +1,50 @@
+from blog.models import Tag, Category, Page, Post
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display: tuple = ('id', 'name', 'slug')
+    list_display_links: tuple = ('name',)
+    search_fields: tuple = ('id', 'name', 'slug')
+    list_per_page: int = 10
+    ordering: tuple = ('-id',)
+    prepopulated_fields: dict = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display: tuple = ('id', 'name', 'slug')
+    list_display_links: tuple = ('name',)
+    search_fields: tuple = ('id', 'name', 'slug')
+    list_per_page: int = 10
+    ordering: tuple = ('-id',)
+    prepopulated_fields: dict = {'slug': ('name',)}
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display: tuple = ('id', 'title', 'is_published')
+    list_display_links: tuple = ('title',)
+    search_fields: tuple = ('id', 'slug', 'title', 'content')
+    list_per_page: int = 50
+    list_filter: tuple = ('is_published',)
+    list_editable: tuple = ('is_published',)
+    ordering: tuple = ('-id',)
+    prepopulated_fields: dict = {'slug': ('title',)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display: tuple = ('id', 'title', 'is_published', 'created_at')
+    list_display_links: tuple = ('title',)
+    search_fields: tuple = ('id', 'slug', 'title', 'excerpt', 'content')
+    list_per_page: int = 50
+    list_filter: tuple = ('category', 'is_published')
+    list_editable: tuple = ('is_published',)
+    ordering: tuple = ('-id',)
+    readonly_fields: tuple = (
+        'created_at', 'updated_at', 'created_by', 'updated_by'
+    )
+    prepopulated_fields: dict = {'slug': ('title',)}
+    autocomplete_fields: tuple = ('category', 'tags')
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,179 @@
-from django.db import models
+"""
+This module contains the `Tag` model, which represents a tag in the
+application.
 
-# Create your models here.
+Classes:
+    Tag
+        A model representing a tag in the application.
+
+Methods:
+    save(*args, **kwargs)
+        Override the default save method to generate a unique slug for the
+        tag if it does not already have one.
+
+"""
+from django.db import models
+from utils.rands import slugify_new
+from django.contrib.auth.models import User
+
+
+class Tag(models.Model):
+    """
+    A model representing a tag in the application.
+
+    Attributes:
+        name (str): The name of the tag.
+        slug (str): A unique slug for the tag.
+    """
+    class Meta:
+        """
+        Meta class for Django models.
+        """
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None, null=True, blank=True,
+        max_length=255,
+    )
+
+    def save(self, *args, **kwargs):
+        """
+        Override the default save method to generate a unique slug for the
+        tag if it does not already have one.
+
+        :param args: Additional positional arguments to be passed to the
+        superclass's save method.
+        :param kwargs: Additional keyword arguments to be passed to the
+        superclass's save method.
+        :return: None
+        """
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Category(models.Model):
+    """
+    A model representing a category in the application.
+
+    Attributes:
+        name (str): The name of the category.
+        slug (str): A unique slug for the category.
+    """
+    class Meta:
+        """
+        Meta class for Django models.
+        """
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None, null=True, blank=True,
+        max_length=255,
+    )
+
+    def save(self, *args, **kwargs):
+        """
+        Override the default save method to generate a unique slug for the
+        category if it does not already have one.
+
+        :param args: Additional positional arguments to be passed to the
+        superclass's save method.
+        :param kwargs: Additional keyword arguments to be passed to the
+        superclass's save method.
+        :return: None
+        """
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Page(models.Model):
+    class Meta:
+        """
+        Meta class for Django models.
+        """
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default="", null=False, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado'
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+
+class Post(models.Model):
+    class Meta:
+        """
+        Meta class for Django models.
+        """
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default="", null=False, blank=True, max_length=255
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado'
+            'para o post ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m',
+                              blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text=('Se marcado, exibirá a capa dentro do post.'),
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='post_created_by'
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return str(self.title)
